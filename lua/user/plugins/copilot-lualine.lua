65fc2ba0663183c4089b3c5a0d514e1d05c44d37
Justin Wong <justin.w.xd@gmail.com>
2024-08-28 11:26:46 +0800

    updates

--- lua/user/plugins/copilot-lualine.lua
+++ lua/user/plugins/copilot-lualine.lua
@@ -1,6 +1,6 @@
-return { 
-	'AndreM222/copilot-lualine',
-	dependencies = { "zbirenbaum/copilot.lua" }, -- or "zbirenbaum/copilot.lua"
-	lazy = true,
-	event = "BufReadPost",
+return {
+  'AndreM222/copilot-lualine',
+  dependencies = { "zbirenbaum/copilot.lua" }, -- or "zbirenbaum/copilot.lua"
+  lazy = true,
+  event = "BufReadPost",
 }

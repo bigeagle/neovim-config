5e2fb1973b1ade9ea1bd34e43693af33ec9688e3
xiongziwen <xiongziwen@msh.team>
2024-12-16 02:53:06 +0000

    terminal color

--- lua/user/plugins/monokai.lua
+++ lua/user/plugins/monokai.lua
@@ -20,6 +20,24 @@ return {
     }
     require("monokai-nightasty").load(opts)
 
+    -- terminal color scheme
+    vim.g.terminal_color_0 = "#151515"
+    vim.g.terminal_color_8 = "#505050"
+    vim.g.terminal_color_1 = "#ac4142"
+    vim.g.terminal_color_9 = "#ac4142"
+    vim.g.terminal_color_2 = "#7e8d50"
+    vim.g.terminal_color_10 = "#7e8d50"
+    vim.g.terminal_color_3 = "#e5b566"
+    vim.g.terminal_color_11 = "#e5b566"
+    vim.g.terminal_color_4 = "#6c99ba"
+    vim.g.terminal_color_12 = "#6c99ba"
+    vim.g.terminal_color_5 = "#9e4e85"
+    vim.g.terminal_color_13 = "#9e4e85"
+    vim.g.terminal_color_6 = "#7dd5cf"
+    vim.g.terminal_color_14 = "#7dd5cf"
+    vim.g.terminal_color_7 = "#d0d0d0"
+    vim.g.terminal_color_15 = "#f5f5f5"
+
   end
 }
 -- vim: ts=2 sts=2 sw=2 expandtab

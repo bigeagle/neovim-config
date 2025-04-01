20ae099a0c74b84a52dba8e3dbae302c5ee3e7cd
xiongziwen <xiongziwen@msh.team>
2025-04-01 16:30:02 +0000

    switch to blink-cmp

---
+++ lua/user/plugins/blink-cmp.lua
@@ -0,0 +1,91 @@
+return {
+  'saghen/blink.cmp',
+  version = '*',
+  dependencies = {
+    "fang2hou/blink-copilot",
+  },
+  opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = {
+      preset = 'default',
+      ['<C-y>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      ['<Tab>'] = {
+        function(cmp)
+          if cmp.snippet_active() then return cmp.accept()
+          else return cmp.select_and_accept() end
+        end,
+        'snippet_forward',
+        'fallback'
+      },
+      ['<CR>'] = { 'accept', 'fallback' },
+    },
+
+    appearance = {
+      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono'
+    },
+
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = {
+      list = {
+        selection = { preselect = true, auto_insert = false }
+      },
+      documentation = { auto_show = true },
+      ghost_text = { enabled = true },
+      menu = {
+        draw = {
+          padding = 2,
+          treesitter = { 'lsp' },
+          columns = {
+            { "label", "label_description", gap = 2 },
+            { "kind_icon", "kind", gap = 1}
+          },
+        }
+      }
+    },
+
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = { 'copilot', 'lsp', 'snippets', 'buffer' },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-copilot",
+          score_offset = 100,
+          async = true,
+          opts = {
+            kind_icon = "",
+          }
+        },
+      },
+    },
+
+    signature = {
+      enabled = true,
+      window = {
+        border = "rounded",
+      }
+    },
+
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+    --
+    -- See the fuzzy documentation for more information
+    fuzzy = { implementation = "prefer_rust_with_warning" }
+  },
+  opts_extend = { "sources.default" }
+}

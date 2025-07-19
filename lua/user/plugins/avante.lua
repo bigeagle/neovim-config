6ff4e24a3813a663f54a073042d369185356e7a2
Justin Wong <justin.w.xd@gmail.com>
2025-07-19 11:00:05 +0800

    upgrade avante

--- lua/user/plugins/avante.lua
+++ lua/user/plugins/avante.lua
@@ -2,7 +2,8 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   lazy = false,
-  version = "0.0.23", -- set this to "*" if you want to always pull the latest change, false to update on release
+  -- version = "0.0.23", -- set this to "*" if you want to always pull the latest change, false to update on release
+  version = "*", -- set this to "*" if you want to always pull the latest change, false to update on release
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
@@ -11,28 +12,9 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
     "zbirenbaum/copilot.lua", -- for providers='copilot'
-    --[[
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
-    --]]
     {
       -- Make sure to set this up properly if you have lazy=true
       'MeanderingProgrammer/render-markdown.nvim',
@@ -44,7 +26,20 @@ return {
   },
   config = function()
     local opts = {
-      provider = "copilot",
+      provider = "moonshot",
+      providers = {
+        moonshot = {
+          __inherited_from = "openai",
+          endpoint = "https://api.moonshot.cn/v1",
+          model = "kimi-k2-0711-preview",
+          api_key_name = 'MOONSHOT_API_KEY',
+          timeout = 30000, -- Timeout in milliseconds
+          extra_request_body = {
+            temperature = 0.6,
+            max_tokens = 32768,
+          },
+        },
+      },
       file_selector = {
         -- file_selector = "fzf",
         provider = "telescope",

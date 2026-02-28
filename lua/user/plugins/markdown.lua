9e35e65ec3a1fc92d448572778aa5a1a66b8f86f
Justin Wong <justin.w.xd@gmail.com>
2026-02-28 10:27:42 +0800

    deprecate avante and claudecode

---
+++ lua/user/plugins/markdown.lua
@@ -0,0 +1,10 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = {
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  },
+  opts = {
+    file_types = { "markdown" },
+  },
+  ft = { "markdown" },
+}
